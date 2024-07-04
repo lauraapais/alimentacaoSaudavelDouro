57ad0cc5c2f1eabebb655e3975d4154d6820ac6a
Laura Pais <pais.laura@gmail.com>
2024-07-04 14:15:07 +0100

    ++

--- nivel2.js
+++ nivel2.js
@@ -415,7 +415,7 @@ class Level {
             textAlign(CENTER);
             rectMode(CENTER);
             let d = dist(mouseX, mouseY, item.pos.x, item.pos.y);
-            if (d < item.item.image.width * itemSize / 2 && !item.plate) {
+            if (d < item.item.image.width * itemSize / 2 && !item.plate &&this.draggingItem==null)  {
                 text(item.name, item.pos.x, item.pos.y + (item.item.image.height * itemSize / 2) + 30);
             }
             pop();
