ad36f9f54a93682f307cf246ba3aa942fc07a315
Laura Pais <pais.laura@gmail.com>
2024-07-05 10:02:44 +0100

    ++

--- nivel1.js
+++ nivel1.js
@@ -466,7 +466,12 @@ class Level {
                 (itemSize + itemSize * item.dragScale / this.timeScaleMax / 10) //Animation Scale
             );
             push();
+            if (w < 900) {
+                textSize(h2Size / 2.2);
+            }
+            else{
             textSize(h2Size / 2.8);
+        }
             textFont(fontBold);
             fill(255);
             textAlign(CENTER);
